a20942a13cf55e4de6663bc1f133775a64a2692c
William Wong <compulim@hotmail.com>
2025-04-17 12:27:48 -0700

    Use Fluent version of Web Chat

--- packages/pages/src/app/ui/ReactWebChatShim.tsx
+++ packages/pages/src/app/ui/ReactWebChatShim.tsx
@@ -1,18 +1,18 @@
-import type ReactWebChat from 'botframework-webchat';
-import React, { useEffect, useRef, type ComponentType } from 'react';
+import type ReactWebChatComponentType from 'botframework-webchat';
+import { type FluentThemeProvider as FluentThemeProviderComponentType } from 'botframework-webchat-fluent-theme';
+import React, { useEffect, useState, type ComponentType } from 'react';
 
 type PropsOf<T> = T extends ComponentType<infer P> ? P : never;
-type Props = PropsOf<typeof ReactWebChat>;
+type Props = PropsOf<typeof ReactWebChatComponentType>;
 
 let loadWebChatJSPromise: Promise<void> | undefined;
 
-async function loadWebChatJS(): Promise<void> {
+async function loadScript(url: string): Promise<void> {
   return new Promise((resolve, reject) => {
     const scriptElement = document.createElement('script');
 
     scriptElement.setAttribute('async', 'async');
-    // scriptElement.setAttribute('src', 'asset/js/webchat.js');
-    scriptElement.setAttribute('src', 'https://unpkg.com/botframework-webchat@main/dist/webchat.js');
+    scriptElement.setAttribute('src', url);
 
     scriptElement.addEventListener('load', () => resolve(), { once: true });
     scriptElement.addEventListener('error', reject, { once: true });
@@ -23,8 +23,18 @@ async function loadWebChatJS(): Promise<void> {
   });
 }
 
+async function loadWebChatJS(): Promise<void> {
+  await loadScript('https://unpkg.com/botframework-webchat@main/dist/webchat.js');
+  await loadScript(
+    'https://unpkg.com/botframework-webchat-fluent-theme@main/dist/botframework-webchat-fluent-theme.production.min.js'
+  );
+}
+
 const ReactWebChatShim = (props: Props) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const [FluentThemeProvider, setFluentThemeProvider] = useState<typeof FluentThemeProviderComponentType | undefined>(
+    undefined
+  );
+  const [WebChat, setWebChat] = useState<typeof ReactWebChatComponentType | undefined>(undefined);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -38,20 +48,24 @@ const ReactWebChatShim = (props: Props) => {
         return;
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (window as any)['WebChat'].renderWebChat(props, ref.current);
-
-      // props.directLine.activity$.subscribe({
-      //   next(value) {
-      //     console.log(value);
-      //   }
-      // });
+      setFluentThemeProvider(() => (window as any).WebChat.FluentThemeProvider);
+      setWebChat(() => (window as any)['WebChat'].ReactWebChat);
     })();
 
     return () => abortController.abort();
-  }, [props]);
+  }, [props, setFluentThemeProvider, setWebChat]);
 
-  return <div ref={ref} style={{ display: 'contents' }} />;
+  console.log(WebChat);
+
+  return (
+    <div style={{ display: 'contents' }}>
+      {FluentThemeProvider && WebChat && (
+        <FluentThemeProvider>
+          <WebChat directLine={props.directLine} />
+        </FluentThemeProvider>
+      )}
+    </div>
+  );
 };
 
 export default ReactWebChatShim;
